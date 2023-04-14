6295e6e99cc6b2375056c9198a3368e5eeebe953
notmaxwang <maxwang3737@gmail.com>
2023-04-14 10:35:55 -0500

    added state for sort

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useState } from "react";
 
 const MovieList = () => {
+
+  // default to popularity
+  const [sort, currSort] = useState('popularity');
+
   return (
     <div>
       <h1>Movie List</h1>
