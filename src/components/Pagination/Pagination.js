7bd3290dbdf0e46f774654633ab5cd173ee5abf1
Guanyu Hou <10951700+vividyu@users.noreply.github.com>
2023-04-18 00:13:15 -0500

    change a ez understanding var name

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import "./Pagination.css";
 
 const Pagination = ({ totalPages, setCurrentPage }) => {
-  const [currentPage, _setCurrentPage] = useState(1);
+  const [currentPage, setLocalCurrentPage] = useState(1);
 
   const handlePreviousPage = () => {
     if (currentPage > 1) {
       const newPage = currentPage - 1;
-      _setCurrentPage(newPage);
+      setLocalCurrentPage(newPage);
       setCurrentPage(newPage);
     }
   };
@@ -15,21 +15,21 @@ const Pagination = ({ totalPages, setCurrentPage }) => {
   const handleNextPage = () => {
     if (currentPage < totalPages) {
       const newPage = currentPage + 1;
-      _setCurrentPage(newPage);
+      setLocalCurrentPage(newPage);
       setCurrentPage(newPage);
     }
   };
 
   return (
     <div className="pagination-container">
-      <button className = "prev-page" onClick={handlePreviousPage} disabled={currentPage === 1}>
-      ←
+      <button className="prev-page" onClick={handlePreviousPage} disabled={currentPage === 1}>
+        ←
       </button>
       <span>
         {currentPage}/{totalPages}
       </span>
-      <button className = "next-page" onClick={handleNextPage} disabled={currentPage === totalPages}>
-      →
+      <button className="next-page" onClick={handleNextPage} disabled={currentPage === totalPages}>
+        →
       </button>
     </div>
   );
