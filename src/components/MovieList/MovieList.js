727f9ceed5c04e9c65c20c8a4db2f356ddf797a4
Michael9705 <yifan.cumecs0709@gmail.com>
2023-04-18 14:40:37 -0700

    Updated CSS styles in MoveList route

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -5,6 +5,7 @@ import MovieItem from "../MovieItem/MovieItem";
 import Pagination from "../Pagination/Pagination";
 import { connect } from "react-redux";
 import { getMovies } from "../../actions/actionCreator";
+import "./MovieList.css";
 
 const MovieList = ({ movieData, blockedMovies, getMovies }) => {
   const [currPage, setCurrPage] = useState(1);
@@ -16,24 +17,33 @@ const MovieList = ({ movieData, blockedMovies, getMovies }) => {
     getMovies(currPage, sortWord);
   }, [currPage, getMovies, sortWord]);
 
-  const totalPages = movieData?.[currPage]?.totalPages || 1;
-  const filteredMovies = movieData?.[currPage]?.movies.filter(
-    (movie) =>
-      !blockedMovies.some((blockedMovie) => blockedMovie.id === movie.id)
-  );
+  const totalPages =
+    movieData && movieData[currPage] && movieData[currPage].totalPages
+      ? movieData[currPage].totalPages
+      : 1;
+  let filteredMovies = [];
+
+  if (movieData && movieData[currPage] && movieData[currPage].movies) {
+    filteredMovies = movieData[currPage].movies.filter(
+      (movie) =>
+        !blockedMovies.some((blockedMovie) => blockedMovie.id === movie.id)
+    );
+  }
 
   return (
     <div>
-      <h1>Movie List</h1>
+      <h1>Our Most Popular Movies!</h1>
       <Pagination
         totalPages={totalPages}
         currentPage={currPage}
         setCurrentPage={setCurrPage}
       />
       <SortBar setSortWord={setSortWord} setCurrPage={setCurrPage} />
-      {filteredMovies?.map((movie) => (
-        <MovieItem key={movie.id} movie={movie} />
-      ))}
+      <div className="list-of-movies">
+        {filteredMovies?.map((movie) => (
+          <MovieItem key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/SortBar/SortBar.js
+++ src/components/SortBar/SortBar.js
@@ -1,19 +1,20 @@
-import './SortBar.css';
+import "./SortBar.css";
 
-const SortBar = ({setSortWord, setCurrPage}) => {
-
+const SortBar = ({ setSortWord, setCurrPage }) => {
   function handleClick(str) {
     setSortWord(str);
     setCurrPage(1);
   }
 
-  return(<div className='sort-bar'>
-     <button onClick={() => handleClick('title')}>Title</button>
-     <button onClick={() => handleClick('release date')}>Release Date</button>
-     <button onClick={() => handleClick('vote average')}>Vote Average</button>
-     <button onClick={() => handleClick('vote count')}>Vote Count</button>
-  </div>);
-}
-
+  return (
+    <div className="sort-bar">
+      <span>Sort By: </span>
+      <button onClick={() => handleClick("title")}>Title</button>
+      <button onClick={() => handleClick("release date")}>Release Date</button>
+      <button onClick={() => handleClick("vote average")}>Vote Average</button>
+      <button onClick={() => handleClick("vote count")}>Vote Count</button>
+    </div>
+  );
+};
 
 export default SortBar;
